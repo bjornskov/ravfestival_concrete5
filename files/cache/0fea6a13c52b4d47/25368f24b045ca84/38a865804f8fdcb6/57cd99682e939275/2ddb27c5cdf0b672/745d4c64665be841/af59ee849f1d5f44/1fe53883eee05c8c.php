d0b029bf299e76e61b81b0eb1ad2c0319f4b29bd
Martin Bjørnskov <mrb@martinb.eu>
2016-05-20 10:23:51 +0200

    first commit

---
+++ files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/af59ee849f1d5f44/1fe53883eee05c8c.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_2ffa68398282bfebfda5b2594d50e416_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1464152368;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1463732184;

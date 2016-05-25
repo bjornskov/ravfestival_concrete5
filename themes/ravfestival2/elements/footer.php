2b130c7499f5d87e8111c87849cf7814d3a1e0f3
Martin Bjørnskov <mrb@martinb.eu>
2016-05-25 13:19:24 +0200

    added social media icons

--- themes/ravfestival2/elements/footer.php
+++ themes/ravfestival2/elements/footer.php
@@ -28,11 +28,9 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
 
                 <div class="footer-col">
                     <ul class="list-inline social-1">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="https://www.facebook.com/ravfestival/"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCrqypLIr5KoqEC918rQKReA"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="https://www.instagram.com/explore/tags/ravfestival/"><i class="fa fa-instagram"></i></a></li>
                     </ul>
                 </div><!--footer-col-->
             </div>
@@ -51,20 +49,10 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
             <div class="col-sm-4">
                 <div class="footer-col">
                     <h3 class="widget-title">
-                        Subscribe to Newsletter
+                        #Ravfestival på Instagram
                     </h3>
-                    <p>
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.
-                    </p>
-                    <form role="form" class="subscribe-form">
-                        <div class="input-group">
-                            <input type="email" class="form-control" placeholder="Enter email to subscribe">
-                                    <span class="input-group-btn">
-                                        <a class="btn  btn-theme btn-lg" href="#">Ok</a>
-                                        <!--<button class="btn  btn-theme btn-lg">Ok</button>-->
-                                    </span>
-                        </div>
-                    </form>
+<!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/7c6485969a405558936edfb66cbc1a5a.html" id="lightwidget_7c6485969a" name="lightwidget_7c6485969a"  scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+
                 </div><!--footer-col-->
             </div>
         </div>
@@ -80,20 +68,12 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
 <a href="#" class="scrollToTop"><i class="fa fa-angle-up"></i></a>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-<!-- Laver easing ved menu -->
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/jquery.easing.1.3.min.js" type="text/javascript"></script>
-<!-- Holder menuen i toppen -->
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/jquery.sticky.js" type="text/javascript"></script>
-<!-- Tæller op i lagkagen -->
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/jquery.counterup.min.js" type="text/javascript"></script>
-<!-- Får elementerne til at komme frem efterhånden -->
-<script src="//amberfestival.dk/application/themes/ravfestival2/js/wow.min.js" type="text/javascript"></script>
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/waypoints.min.js" type="text/javascript"></script>
-<!-- Paralax på billederne -->
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/jquery.stellar.min.js" type="text/javascript"></script>
-<!-- Diverse scripts der aktiverer funktioner -->
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/custom.js" type="text/javascript"></script>
-<!-- Video background script i header. -->
 <script src="//amberfestival.dk/application/themes/ravfestival2/js/jquery.vide.js"></script>
 
 

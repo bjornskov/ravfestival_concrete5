7886e69119d33855e08eabe5c2bf00176602e6bc
Martin Bjørnskov <mrb@martinb.eu>
2016-05-25 13:18:41 +0200

    remove unused shit

--- themes/ravfestival2/js/custom.js
+++ themes/ravfestival2/js/custom.js
@@ -60,20 +60,6 @@ jQuery(document).ready(function ($) {
     });
 });
 
-/* ==============================================
- WOW plugin triggers animate.css on scroll
- =============================================== */
-
-var wow = new WOW(
-        {
-            boxClass: 'wow', // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset: 100, // distance to the element when triggering the animation (default is 0)
-            mobile: false        // trigger animations on mobile devices (true is default)
-        }
-);
-wow.init();
-
 
 
 //smooth scroll
@@ -92,9 +78,6 @@ $(function () {
         }
     });
 });
-
-
-
 
 
 
